3f224dc317267b7b4f4491376b674680c50d9753
Artem Zapara <art.zapara@gmail.com>
2024-11-20 15:09:50 +0100

    created script for field drawing

---
+++ src/field/draw.py
@@ -0,0 +1,51 @@
+import argparse
+from pathlib import Path
+import plotly.graph_objects as go
+
+
+def parse_arguments() -> argparse.Namespace:
+    parser = argparse.ArgumentParser(
+        description="Generate a field image and save it."
+    )
+    parser.add_argument(
+        "--output_dir",
+        type=str,
+        default="./src/resources",
+        help="Directory to save the image"
+    )
+    parser.add_argument(
+        "--file_name",
+        type=str,
+        default="field.png",
+        help="Filename for the field image"
+    )
+    return parser.parse_args()
+
+
+def ensure_directory_exists(path: Path):
+    path.parent.mkdir(
+        parents=True,
+        exist_ok=True
+    )
+
+def draw(path: Path):
+    ensure_directory_exists(
+        path=path
+    )
+
+    fig = go.Figure()
+    fig.write_image(
+        file=path
+    )
+
+def main():
+    args: argparse.Namespace = parse_arguments()
+    path: Path = Path(args.output_dir) / args.file_name
+    if path.exists():
+        print(f"File {path} exists.")
+    else:
+        print(f"File {path} does not exist. Drawing...")
+        draw(path=path)
+
+if __name__ == "__main__":
+    main()
